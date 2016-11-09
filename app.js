20e5c6518ef700821d0bc7fce945d9679c3ef023
wei <zhongjia_wei@163.com>
2016-11-09 23:07:55 +0800

    声明外部依赖

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/wechat', wechatIotParse);
+app.use('/wechat', wechatIotParse({token:"jgt44Gn2qVbd26k6B8mbU8nXqBkbnUQ6"}));
 app.use('/', index);
 app.use('/users', users);
 
