4942c7efe84259d65c7be3cab2263c5acd4604c3
wei <zhongjia_wei@163.com>
2016-12-01 22:26:53 +0800

    先不依赖parser

---
+++ server/message-parser.js
@@ -0,0 +1,34 @@
+var crypto = require("crypto");
+
+var checkSignature = function (signature, timestamp, nonce, token) {
+
+    var shasum = crypto.createHash('sha1');
+    var arr = [token, timestamp, nonce].sort();
+    shasum.update(arr.join(''));
+
+    return shasum.digest('hex') === signature;
+};
+
+
+
+var wechat = function(config){
+    return function (req, res) {
+        if(req.method == "GET"){
+            if(checkSignature(req.query.signature, req.query.timestamp, req.query.nonce, config.token)){
+                res.send(req.query.echostr);
+            }else{
+                res.send("error");
+            }
+        }else{
+            var db
+            var collection = db.collection('wechat-message');
+            collection.insertOne(req.body, function(err, result) {
+                console.log("Inserted wechat-message into the collection");
+                res.json({error_code: 0, error_msg: "ok"});
+            });
+        }
+    }
+
+}
+
+module.exports = wechat;
