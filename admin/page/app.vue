b4db663a79cfabf6f64e0e2c781fdbf70474ef69
supermanOnHorse <948913886@qq.com>
2016-12-03 15:31:24 +0800

    head样式

--- admin/page/app.vue
+++ admin/page/app.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
-        <div style="background: #20A0FF;height: 80px;color: white;padding:20px;">
-            <h1>智能车锁后台</h1>
+        <div style="background: #20A0FF;height: 80px;color: white;padding:20px;box-sizing: border-box;">
+            <h1 style="margin: 0;">智能车锁后台</h1>
 
         </div>
         <el-row class="tac">
