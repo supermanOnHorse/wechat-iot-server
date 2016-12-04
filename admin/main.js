0448522d3a11f0431c27376352b67916747261a4
supermanOnHorse <948913886@qq.com>
2016-12-04 19:48:23 +0800

    设备激活

--- admin/main.js
+++ admin/main.js
@@ -4,7 +4,8 @@ import 'element-ui/lib/theme-default/index.css';
 import App from './page/app.vue';
 import VueRouter from 'vue-router';
 import Product from './page/product.vue';
-import VueResource from 'vue-resource'
+import VueResource from 'vue-resource';
+import Device from './page/device.vue';
 
 
 Vue.use(ElementUI);
@@ -12,10 +13,13 @@ Vue.use(VueRouter);
 Vue.use(VueResource);
 
 const routes = [
-  {
-    path:'/product',
-    component: Product
-  }
+    {
+        path:'/product',
+        component: Product
+    },{
+        path:'/device/:product_id',
+        component: Device
+    }
 ];
 const router = new VueRouter({routes});
 new Vue({
